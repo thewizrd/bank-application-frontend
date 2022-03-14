88a95c2553859641957508cd6b7e215d8f9096db
Dave Antoine <thewizrd.dev@gmail.com>
2022-03-14 17:32:21 -0400

    Update components
    * Header/footer
    * Landing/home page
    * Login (Customer/Staff-Admin)
    * Admin: Register Staff
    * Admin: View all
    * Customer: register, update profile

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterStaffComponent } from './register-staff/register-staff.component';
+import { ViewStaffComponent } from './view-staff/view-staff.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'admin/registerStaff',
+    component: RegisterStaffComponent,
+  },
+  {
+    path: 'admin/viewStaff',
+    component: ViewStaffComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AdminRoutingModule { }
+export class AdminRoutingModule {}
--- src/app/customer/login/login.component.ts
+++ src/app/customer/login/login.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  loginForm: any = {};
+  message: string = '';
+  errorMsg: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

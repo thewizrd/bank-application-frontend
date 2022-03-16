b1ffc9e017eae7929e13f8b3ecc3aed7488ba64a
emarquez1993 <53238046+emarquez1993@users.noreply.github.com>
2022-03-16 10:33:01 -0500

    Account lookup for staff.

--- src/app/staff/customer-accounts/customer-accounts.component.ts
+++ src/app/staff/customer-accounts/customer-accounts.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AllAccountsResponse } from 'src/app/interfaces/all-accounts-response';
+import { AccountLookupResponse } from 'src/app/interfaces/account-lookup-response';
 import { StaffService } from 'src/app/services/staff.service';
 
 @Component({
@@ -8,13 +8,27 @@ import { StaffService } from 'src/app/services/staff.service';
   styleUrls: ['./customer-accounts.component.css'],
 })
 export class CustomerAccountsComponent implements OnInit {
-  accounts: AllAccountsResponse[] = [];
+  accountNumber: any;
+  customerName: any;
+  accountBalance: any;
+
+  accounts!: AccountLookupResponse;
 
   constructor(private _staffService: StaffService) {}
 
-  ngOnInit(): void {
-    this.reloadData();
+  viewAccounts(): void {
+    this._staffService.getAccountByAccountNumber(this.accountNumber).subscribe({
+      next: (result) => {
+        this.accounts = result;
+        this.customerName = this.accounts.firstName
+          .concat(' ')
+          .concat(this.accounts.lastName);
+        this.accountBalance = this.accounts.balance;
+      },
+    });
   }
 
-  reloadData() {}
+  ngOnInit(): void {}
+
+  reloadTable() {}
 }

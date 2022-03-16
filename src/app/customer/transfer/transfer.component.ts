b0a6a136e91c45d35916be7137e7d78d7bb1df59
emarquez1993 <53238046+emarquez1993@users.noreply.github.com>
2022-03-15 22:32:26 -0500

    Utilized ng model in transfers

--- src/app/customer/transfer/transfer.component.ts
+++ src/app/customer/transfer/transfer.component.ts
@@ -9,6 +9,11 @@ import { TokenStorageService } from 'src/app/services/token-storage.service';
   styleUrls: ['./transfer.component.css'],
 })
 export class TransferComponent implements OnInit {
+  fromAccNumber: any;
+  toAccNumber: any;
+  amount: any;
+  reason: any;
+
   constructor(
     private _customerService: CustomerService,
     private _tokenStorageService: TokenStorageService
@@ -23,15 +28,10 @@ export class TransferComponent implements OnInit {
       console.log('Unable to verify customer.');
     } else {
       request.by = token.id;
-      request.fromAccNumber = parseInt(
-        (<HTMLInputElement>document.getElementById('fromAccNumber')).value
-      );
-      request.reason = (<HTMLInputElement>(
-        document.getElementById('reason')
-      )).value;
-      request.toAccNumber = parseInt(
-        (<HTMLInputElement>document.getElementById('toAccNumber')).value
-      );
+      request.fromAccNumber = this.fromAccNumber;
+      request.reason = this.reason;
+      request.toAccNumber = this.toAccNumber;
+      request.amount = this.amount;
       this._customerService.doTransfer(request).subscribe({
         next: (result) => {
           alert('Transfer Successful');

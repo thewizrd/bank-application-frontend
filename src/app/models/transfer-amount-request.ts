697ec7398e26e6bb474a0def182025ea4a581e74
Dave Antoine <thewizrd.dev@gmail.com>
2022-03-17 18:29:49 -0400

    transfers: add input for transaction type

--- src/app/models/transfer-amount-request.ts
+++ src/app/models/transfer-amount-request.ts
@@ -1,7 +1,11 @@
+import { TransactionType } from '../enums/transaction-type';
+
 export class TransferAmountRequest {
   fromAccNumber: number | null = null;
   toAccNumber: number | null = null;
   amount: number | null = null;
   reason: string | null = null;
   byStaff: string | null = null;
+
+  transactionType: TransactionType | null = TransactionType.DEBIT;
 }
--- src/app/models/transfer-request.ts
+++ src/app/models/transfer-request.ts
@@ -1,7 +1,11 @@
+import { TransactionType } from '../enums/transaction-type';
+
 export class TransferRequest {
   fromAccNumber: number | null = null;
   toAccNumber: number | null = null;
   amount: number | null = null;
   reason: string | null = null;
   by: number | null = null;
+
+  transactionType: TransactionType | null = TransactionType.DEBIT;
 }
